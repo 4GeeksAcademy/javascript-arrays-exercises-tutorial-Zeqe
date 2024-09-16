e5353ae203f06582ae2000fc9deaa9454220dfc9
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-09-16 16:06:49 +0000

    Terminando el Practice Tutorial completo

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,9 +1,28 @@
 // Your code here
+function lyricsGenerator(array) {
+    let song = "";
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 1) {
+            count++;
+            song += "Drop the bass ";
+            if (count === 3) {
+                song += "!!!Break the bass!!! ";
+                count = 0;
+            };
+        } else{
+            song += 'Boom ';
+            count = 0;
+        } 
+    }
+    return song.trim()
+    }
+
 
 
 // Don't change anything below this line
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+console.log(lyricsGenerator([0, 0, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 1, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 0]))
+console.log(lyricsGenerator([1, 0, 1]))
+console.log(lyricsGenerator([1, 1, 1]))
